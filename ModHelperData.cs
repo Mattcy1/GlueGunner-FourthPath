87c1053cc527e763f8964fcfa574547b7ec485b0
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2023-08-17 13:07:11 +0200

    Add files via upload

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,9 +3,9 @@ namespace GlueGunnerFourthPath;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "38.3";
-    public const string Version = "1.1";
+    public const string Version = "1.2";
     public const string Name = "Glue Gunner Fourth Path";
-    public const string Description = "Glue gunner fourth path";
+    public const string Description = "Glue gunner fourth path can now hit moab";
     public const string RepoOwner = "Mattcy1";
     public const string RepoName = "Glue Gunner Fourth Path";
     public const string Icon = "GlueGunnerFourthPath-Portrait.png";
